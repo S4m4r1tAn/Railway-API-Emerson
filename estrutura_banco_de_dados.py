f0d421d292db96d6262dd1178f2476b0523385dd
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-09 17:54:45 +0000

    user update

--- estrutura_banco_de_dados.py
+++ estrutura_banco_de_dados.py
@@ -37,7 +37,7 @@ def inicializar_banco():
         db.drop_all()
         db.create_all()
         # Criar usuários adminstradores
-        autor = Autor(nome='S4m4r1t4N', email='s4m4r1t4n@email.com',
+        autor = Autor(nome='Emerson', email='emerson@email.com',
                     senha='123456', admin=True)
         db.session.add(autor)
         db.session.commit()
